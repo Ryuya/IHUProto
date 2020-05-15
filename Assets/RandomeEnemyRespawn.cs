38b792cedc09173353a0ff706cf375809f3b7866
Ryuya <main.higashi.pg@gmail.com>
2020-05-16 04:37:35 +0900

    first commit

---
+++ Assets/RandomeEnemyRespawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomeEnemyRespawn : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject obj = (GameObject)Resources.Load ("Prefabs/Test_98vertex");
+
+        for (int x = 0; x < 250; x++)
+        {
+            var x_pos = (float)Random.Range(-125,125);
+            var z_pos = (float)Random.Range(0,250);
+            // プレハブを元にオブジェクトを生成する
+                GameObject instance = (GameObject) Instantiate(obj,
+                    new Vector3(x_pos, 0.0f, z_pos),
+                    Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private float speed = 0.3f;
+    private float i = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i += Time.deltaTime * speed;
+        this.transform.Rotate(0f,i,0f);
+    }
+}
